b278f19155e51baca57ef99e913a9d7cbefcf412
Zakariya Bazaou <z.bazaou@outlook.fr>
2020-07-01 21:02:08 +0200

    canvas noir et balle blanche

--- pong.js
+++ pong.js
@@ -1,13 +1,24 @@
 const canvas = document.getElementById("pong");
 const context = canvas.getContext("2d");
 
-context.fillStyle = "black";
-context.fillRect(100,200,50,75);
 
-context.fillStyle = "red";
-context.beginPath();
-context.arc(300,350,100,0,Math.PI*2,false);
+// dessin du canvas
+function dessRect(x,y,w,h,color){
+    context.fillStyle = color;
+    context.fillRect(x,y,w,h);
+}
+dessRect(0,0,canvas.width,canvas.height,"black" );
 
-context.closePath();
-context.fill();
+//dessin du cercle
+
+function dessCercle(x,y,r,color){
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(x,y,r,0,Math.PI*2,false);
+    context.closePath();
+    context.fill();
+}
+dessCercle(100,100,20,"white");
+
+
 
