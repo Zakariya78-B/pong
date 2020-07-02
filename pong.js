22f927650a0d2dd6a032a87c13ab2c6c1cf99273
Zakariya Bazaou <z.bazaou@outlook.fr>
2020-07-02 23:00:01 +0200

    commit jeu de pong affichage raquette score et balle

--- pong.js
+++ pong.js
@@ -38,7 +38,21 @@ function dessRect(x,y,w,h,color){
     context.fillStyle = color;
     context.fillRect(x,y,w,h);
 }
-dessRect(0,0,canvas.width,canvas.height,"black" );
+// creation du filet
+const filet = {
+    x : canvas.width - 1,
+    y : 0,
+    width : 2,
+    height: 10,
+    color: "white"
+}
+//dessin du filet
+function dessFilet(){
+    for(let i = 0;i <= canvas.height;i+=15){
+        dessRect(filet.x,filet.y + 1,filet.width,filet.height,filet.color);
+    }
+
+}
 
 //dessin du cercle
 
@@ -49,7 +63,7 @@ function dessCercle(x,y,r,color){
     context.closePath();
     context.fill();
 }
-dessCercle(100,100,20,"white");
+
 
 
 // style du texte
@@ -58,4 +72,28 @@ function dessTexte(text,x,y,color){
     context.font = "45px fantasy";
     context.fillText(text,x,y);
 }
-dessTexte("Test",300,200,"white");
+// fonction engagement
+
+function engagement(){
+    dessRect(0,0,canvas.width,canvas.height,"Black");
+
+    dessFilet();
+
+    dessTexte(zak.score,canvas.width/4,canvas.height/5,"white");
+    dessTexte(com.score,3*canvas.width/4,canvas.height/5,"white");
+
+    dessRect(zak.x,zak.y,zak.width,zak.height,zak.color);
+    dessRect(com.x,com.y,com.width,com.height,com.color);
+
+    dessCercle(balle.x,balle.y,balle.radius,balle.color);
+
+}
+//initialisation du jeu
+function jeu(){
+    engagement();
+}
+//affichage mouvement
+const imageParSeconde = 50;
+setInterval(jeu,1000/imageParSeconde);
+
+
